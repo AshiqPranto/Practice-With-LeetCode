86d0740f888425168562ec99fdce7c89b57c156e
Ashiq Pranto <ashiqpranto@gmail.com>
2022-04-16 17:12:39 +0600

    Time: 31 ms (96.06%), Space: 33.5 MB (62.62%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -11,41 +11,18 @@
  */
 class Solution {
 public:
+    int sum = 0;
+    void dfs(TreeNode* x)
+    {
+        if(x->right !=nullptr)  dfs(x->right);
+        sum+=x->val;
+        x->val = sum;
+        if(x->left!=nullptr) dfs(x->left);
+    }
+    
     TreeNode* convertBST(TreeNode* root) {
-        vector<int> a;
-        TreeNode* temp = root;
-        queue<TreeNode*> q;
-        q.push(temp);
-        while(!q.empty())
-        {
-            temp = q.front();
-            q.pop();
-            if(temp==nullptr) return root;
-            a.push_back(temp->val);
-            if(temp->left != nullptr) q.push(temp->left);
-            if(temp->right != nullptr) q.push(temp->right);
-        }
-        sort(a.begin(),a.end());
-        vector<int>cumsum(a.size(),0);
-        if(a.size()>0) cumsum[a.size()-1] = a[a.size()-1];
-        for(int i = a.size()-2;i>=0;i--)
-        {
-            cumsum[i] = cumsum[i+1]+a[i];
-        }
-        q.push(root);
-        while(!q.empty())
-        {
-            temp = q.front();
-            q.pop();
-            int index = upper_bound(a.begin(),a.end(),temp->val)-a.begin();
-            if(index<a.size()) 
-            {
-                temp->val = cumsum[index]+temp->val;
-            }
-            if(temp->left != nullptr) q.push(temp->left);
-            if(temp->right != nullptr) q.push(temp->right);
-        }
+        if(root == nullptr) return root;
+        dfs(root);
         return root;
-        
     }
 };
